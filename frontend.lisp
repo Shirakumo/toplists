27f32a11e70628c5e68e4296ec8492d79bed0710
Shinmera <shinmera@tymoon.eu>
2018-03-08 16:54:47 +0100

    Preliminary frontend separation and other things.

--- frontend.lisp
+++ frontend.lisp
@@ -5,3 +5,43 @@
 |#
 
 (in-package #:org.shirakumo.radiance.toplists)
+
+(define-page all "toplists/list" (:clip "all.ctml")
+  (check-permission 'view)
+  (let ((lists (dm:get 'lists (db:query :all))))
+    (r-clip:process T :lists lists)))
+
+(define-page new-list "toplists/new" (:clip "edit.ctml")
+  (check-permission 'new)
+  (r-clip:process T))
+
+(define-page view-list "toplists/(.+)" (:uri-groups (list) :clip "view.ctml")
+  (let* ((list (ensure-list list))
+         (items (list-items list)))
+    (check-permission 'view list)
+    (r-clip:process T :title (dm:field list "title")
+                      :items items)))
+
+(define-page edit-list "toplists/(.+)/edit" (:uri-groups (list) :clip "edit.ctml")
+  (let* ((list (ensure-list list))
+         (items (list-items list)))
+    (check-permission '(edit delete) list)
+    (r-clip:process T :title (dm:field list "title")
+                      :items items)))
+
+(define-page new-order "toplists/(.+)/new" (:uri-groups (list) :clip "view.ctml")
+  (let* ((list (ensure-list list))
+         (items (list-items list)))
+    (check-permission 'new list)
+    (r-clip:process T :title (dm:field list "title")
+                      :editable T
+                      :items items)))
+
+(define-page view-order "toplists/(.+)/(.+)" (:uri-groups (list order) :clip "view.ctml")
+  (let* ((list (ensure-list list))
+         (order (ensure-order order))
+         (items (list-items (dm:id list))))
+    (check-permission 'view list)
+    (r-clip:process T :title (dm:field list "title")
+                      :editable T ;; FIXME: Check author
+                      :items items)))
--- static/toplists.js
+++ static/toplists.js
@@ -1,161 +1,166 @@
-var Toplists = function(list){
+var Toplist = function(list){
     var self = this;
-    list = list || document.getElementById("list");
-    var images = list.querySelector(".images");
-    var items = list.querySelector(".items");
-
-    self.idIndex = (root, id)=>{
-        var i = 0;
-        for(var el of root.children){
-            if(el.dataset.id == id) return i;
-            i++;
-        }
-        return -1;
-    };
-
-    self.sortChildren = (container, score)=>{
-        const items = [...container.children];
-
-        items.sort((a, b) => score(a) - score(b))
-            .forEach(item => container.appendChild(item));
-        return container;
-    };
-
-    self.saveOrder = ()=>{
-        var order = [];
-        for(var el of images.children){
-            order.push(el.dataset.id);
-        }
-        window.localStorage.setItem("order", JSON.stringify(order));
-        return order;
-    };
-
-    self.loadOrder = ()=>{
-        var order = JSON.parse(window.localStorage.getItem("order")||"[]");
-        self.sortChildren(images, (el)=> order.indexOf(el.dataset.id));
-        self.sortChildren(items, (el)=> order.indexOf(el.dataset.id));
-        return images;
-    };
-
-    self.imageForSorter = (sorter) =>{
-        return images.children[self.idIndex(images, sorter.dataset.id)];
-    };
-
-    self.makeImageActive = (image) =>{
-        if(!image.classList.contains("active")){
-            [].slice.call(images.children).forEach((img)=>img.classList.remove("active"));
-            image.classList.add("active");
-        }
-        return image;
-    }
-
-    self.makeDraggable = (root, onBegin, onMove, onFinish)=>{
-        var drag = null;
-        var onStart = null;
-        var onDrag = null;
-        var onEnd = null;
-        var onHover = null;
-        var onUnhover = null;
-
-        onStart = (ev)=>{
-            drag = ev.target;
-            ev.dataTransfer.effectAllowed = "move";
-            setTimeout(()=>{drag.classList.add("ghost");}, 0);
-            if(onBegin) onBegin(drag);
-            root.removeEventListener("mouseover", onHover);
-            root.removeEventListener("mouseout", onUnhover);
-        };
-
-        onDrag = (ev)=>{
-            if(!drag) return;
-            ev.preventDefault();
-            ev.dataTransfer.dropEffect = "move";
-            var target = ev.target;
-            if(target && target !== drag){
-                if(target.parentNode == root)
-                    root.insertBefore(drag, target);
-                else
-                    root.appendChild(drag);
-            }
-            if(onMove) onMove(drag);
-        };
-
-        onEnd = (ev)=>{
-            ev.preventDefault();
-            drag.classList.remove("ghost");
-            if(onFinish) onFinish(drag);
-            drag = null;
-            root.addEventListener("mouseover", onHover, false);
-            root.addEventListener("mouseout", onUnhover, false);
-        };
-
-        onHover = (ev)=>{
-            if(onBegin && ev.target.parentNode == root)
-                onBegin(ev.target);
-        };
-
-        onUnhover = (ev)=>{
-            if(onFinish && ev.target.parentNode == root)
-                onFinish(ev.target);
-        };
-        
-        root.addEventListener("dragstart", onStart, false);
-        root.addEventListener("dragover", onDrag, false);
-        root.addEventListener("dragend", onEnd, false);
-        root.addEventListener("mouseover", onHover, false);
-        root.addEventListener("mouseout", onUnhover, false);
-        [].slice.call(root.children).forEach((el)=>{el.draggable = true;});
-        return root;
-    };
-
-    self.init = ()=>{
-        if(list.classList.contains("editable")){
-            self.loadOrder();
-            self.makeDraggable(items, (el)=>{
-                self.makeImageActive(self.imageForSorter(el));
-            }, (el)=>{
-                self.sortChildren(images, (el)=>{
-                    return self.idIndex(items, el.dataset.id);
-                });
-            }, (el)=>{
-                self.imageForSorter(el).classList.remove("active");
-                self.saveOrder();
-            });
-
-            self.makeDraggable(images, (el)=>{
-                self.makeImageActive(el);
-            }, (el)=>{
-                self.sortChildren(items, (el)=>{
-                    return self.idIndex(images, el.dataset.id);
-                });
-            }, (el)=>{
-                el.classList.remove("active");
-                self.saveOrder();
-            });
-        } else {
-            items.addEventListener("mouseover", (ev)=>{
-                if(ev.target.parentNode == items) {
-                    self.makeImageActive(self.imageForSorter(ev.target));
-                }
-            }, false);
-            items.addEventListener("mouseout", (ev)=>{
-                if(ev.target.parentNode == items) {
-                    self.imageForSorter(ev.target).classList.remove("active");
-                }
-            }, false);
-            images.addEventListener("mouseover", (ev)=>{
-                if(ev.target.parentNode == images) {
-                    self.makeImageActive(ev.target);
-                }
-            }, false);
-            images.addEventListener("mouseout", (ev)=>{
-                if(ev.target.parentNode == images) {
-                    ev.target.classList.remove("active");
-                }
-            }, false);
-        }
-    }
-
+    self.images = list.querySelector(".images");
+    self.items = list.querySelector(".items");
     self.init();
 };
-var toplists = new Toplists();
+
+Toplist.prototype.idIndex = (root, id)=>{
+    var i = 0;
+    for(var el of root.children){
+        if(el.dataset.id == id) return i;
+        i++;
+    }
+    return -1;
+};
+
+Toplist.prototype.sortChildren = (container, score)=>{
+    var items = [...container.children];
+    
+    items.sort((a, b) => score(a) - score(b))
+        .forEach(item => container.appendChild(item));
+    return container;
+};
+
+Toplist.prototype.saveOrder = ()=>{
+    var order = [];
+    for(var el of self.images.children){
+        order.push(el.dataset.id);
+    }
+    window.localStorage.setItem("order", JSON.stringify(order));
+    return order;
+};
+
+Toplist.prototype.loadOrder = ()=>{
+    var order = JSON.parse(window.localStorage.getItem("order")||"[]");
+    this.sortChildren(this.images, (el)=> order.indexOf(el.dataset.id));
+    this.sortChildren(this.items, (el)=> order.indexOf(el.dataset.id));
+    return images;
+};
+
+Toplist.prototype.imageForSorter = (sorter) =>{
+    return self.images.children[this.idIndex(images, sorter.dataset.id)];
+};
+
+Toplist.prototype.makeImageActive = (image) =>{
+    if(!image.classList.contains("active")){
+        [].slice.call(self.images.children).forEach((img)=>img.classList.remove("active"));
+        image.classList.add("active");
+    }
+    return image;
+}
+
+Toplist.prototype.makeDraggable = (root, onBegin, onMove, onFinish)=>{
+    var drag = null;
+    var onStart = null;
+    var onDrag = null;
+    var onEnd = null;
+    var onHover = null;
+    var onUnhover = null;
+
+    onStart = (ev)=>{
+        drag = ev.target;
+        ev.dataTransfer.effectAllowed = "move";
+        setTimeout(()=>{drag.classList.add("ghost");}, 0);
+        if(onBegin) onBegin(drag);
+        root.removeEventListener("mouseover", onHover);
+        root.removeEventListener("mouseout", onUnhover);
+    };
+
+    onDrag = (ev)=>{
+        if(!drag) return;
+        ev.preventDefault();
+        ev.dataTransfer.dropEffect = "move";
+        var target = ev.target;
+        if(target && target !== drag){
+            if(target.parentNode == root)
+                root.insertBefore(drag, target);
+            else
+                root.appendChild(drag);
+        }
+        if(onMove) onMove(drag);
+    };
+
+    onEnd = (ev)=>{
+        ev.preventDefault();
+        drag.classList.remove("ghost");
+        if(onFinish) onFinish(drag);
+        drag = null;
+        root.addEventListener("mouseover", onHover, false);
+        root.addEventListener("mouseout", onUnhover, false);
+    };
+
+    onHover = (ev)=>{
+        if(onBegin && ev.target.parentNode == root)
+            onBegin(ev.target);
+    };
+
+    onUnhover = (ev)=>{
+        if(onFinish && ev.target.parentNode == root)
+            onFinish(ev.target);
+    };
+    
+    root.addEventListener("dragstart", onStart, false);
+    root.addEventListener("dragover", onDrag, false);
+    root.addEventListener("dragend", onEnd, false);
+    root.addEventListener("mouseover", onHover, false);
+    root.addEventListener("mouseout", onUnhover, false);
+    [].slice.call(root.children).forEach((el)=>{el.draggable = true;});
+    return root;
+};
+
+Toplist.prototype.init = ()=>{
+    var self = this;
+    if(list.classList.contains("editable")){
+        self.loadOrder();
+        self.makeDraggable(self.items, (el)=>{
+            self.makeImageActive(self.imageForSorter(el));
+        }, (el)=>{
+            self.sortChildren(self.images, (el)=>{
+                return self.idIndex(self.items, el.dataset.id);
+            });
+        }, (el)=>{
+            self.imageForSorter(el).classList.remove("active");
+            self.saveOrder();
+        });
+
+        self.makeDraggable(self.images, (el)=>{
+            self.makeImageActive(el);
+        }, (el)=>{
+            self.sortChildren(self.items, (el)=>{
+                return self.idIndex(self.images, el.dataset.id);
+            });
+        }, (el)=>{
+            el.classList.remove("active");
+            self.saveOrder();
+        });
+    } else {
+        self.items.addEventListener("mouseover", (ev)=>{
+            if(ev.target.parentNode == self.items) {
+                self.makeImageActive(self.imageForSorter(ev.target));
+            }
+        }, false);
+        self.items.addEventListener("mouseout", (ev)=>{
+            if(ev.target.parentNode == self.items) {
+                self.imageForSorter(ev.target).classList.remove("active");
+            }
+        }, false);
+        self.images.addEventListener("mouseover", (ev)=>{
+            if(ev.target.parentNode == self.images) {
+                self.makeImageActive(ev.target);
+            }
+        }, false);
+        self.images.addEventListener("mouseout", (ev)=>{
+            if(ev.target.parentNode == self.images) {
+                ev.target.classList.remove("active");
+            }
+        }, false);
+    }
+}
+
+document.addEventListener("DOMContentReady", ()=>{
+    var lists = document.querySelectorAll(".list");
+    for(var i=0; i<lists.length; i++){
+        new Toplist(lists[i]);
+    }
+}, false);
